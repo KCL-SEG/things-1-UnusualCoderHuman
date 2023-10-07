80243ae94b28dd9d35269bfddedf94ea7bb77f17
Aryav_Lahiri <aryav.lahiri@kcl.ac.uk>
2023-10-07 20:49:17 +0100

    Add basic model to things app

--- things/models.py
+++ things/models.py
@@ -1,3 +1,13 @@
+from django.core.validators import RegexValidator
+from django.contrib.auth.models import AbstractUser
 from django.db import models
-
 # Create your models here.
+
+
+class Thing(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    quantity = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
