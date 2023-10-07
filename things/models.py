e75067df85cb13058dd0ecd54fda36f5acb87e83
Aryav_Lahiri <aryav.lahiri@kcl.ac.uk>
2023-10-07 21:03:43 +0100

    Add constraints to attributes in model

--- things/models.py
+++ things/models.py
@@ -1,13 +1,24 @@
 from django.core.validators import RegexValidator
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 # Create your models here.
 
 
 class Thing(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.TextField()
-    quantity = models.PositiveIntegerField()
+    name = models.CharField(
+        max_length = 30,
+        unique = True,
+        blank = False)
+    description = models.TextField(
+        max_length = 120
+    )
+    quantity = models.PositiveIntegerField(
+        validators=[
+            MinValueValidator(0),
+            MaxValueValidator(100)
+        ]
+    )
 
     def __str__(self):
         return self.name
