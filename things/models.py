ba6939009ed6739d3a1efa1963f2becc5eb22a86
Aryav_Lahiri <aryav.lahiri@kcl.ac.uk>
2023-10-09 23:38:47 +0100

    Allow description to be blank and non-unique and quantity to b non- unique

--- things/models.py
+++ things/models.py
@@ -10,14 +10,19 @@ class Thing(models.Model):
         max_length = 30,
         unique = True,
         blank = False)
+    
     description = models.TextField(
-        max_length = 120
+        max_length = 120, 
+        blank = True,
+        unique = False
     )
+
     quantity = models.PositiveIntegerField(
         validators=[
             MinValueValidator(0),
             MaxValueValidator(100)
-        ]
+        ],
+        unique = False
     )
 
     def __str__(self):
